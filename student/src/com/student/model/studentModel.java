4ac3ff813feaa7bb2916c5c97f8f941462829b4e
liaogaoying2019 <780566532@qq.com>
2019-05-14 10:35:46 +0800

    提交到本地仓库

---
+++ student/src/com/student/model/studentModel.java
@@ -0,0 +1,16 @@
+package com.student.model;
+
+public class studentModel {
+	private int id;
+	private String username;
+	private String password;
+	private int sex;
+	public studentModel(int id, String username, String password, int sex) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.sex = sex;
+	}
+	
+}
